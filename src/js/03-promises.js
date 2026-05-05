b3cbf596b02bb08bdf5f902e9cb5bed3a2b3e20c
Rostislav <125198729+Zelenro@users.noreply.github.com>
2026-05-05 12:18:08 +0300

    task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,133 @@
+'use strict';
+
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 3000,
+  messageMaxLength: 110,
+  backOverlay: false,
+  backOverlayColor: 'rgba(0,0,0,0.5)',
+  plainText: true,
+  showOnlyTheLastOne: false,
+  clickToClose: false,
+  pauseOnHover: true,
+  ID: 'NotiflixNotify',
+  className: 'notiflix-notify',
+  zindex: 4001,
+  fontFamily: 'Quicksand',
+  fontSize: '13px',
+  cssAnimation: true,
+  cssAnimationDuration: 1400,
+  cssAnimationStyle: 'fade',
+  closeButton: false,
+  useIcon: true,
+  useFontAwesome: false,
+  fontAwesomeIconStyle: 'basic',
+  fontAwesomeIconSize: '34px',
+  success: {
+    background: '#325cc6',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-success',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-check-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(50,198,130,0.2)',
+  },
+  failure: {
+    background: '#ff5549',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  },
+  warning: {
+    background: '#eebf31',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-warning',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-exclamation-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(238,191,49,0.2)',
+  },
+  info: {
+    background: '#26c0d3',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-info',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-info-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(38,192,211,0.2)',
+  },
+});
+
 const formRef = document.querySelector('form');
+
+let intervalId = null;
+let timeoutId = null;
+let position = 0;
+let delay = 0;
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  })
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+function createInterval(step, amount) {
+  intervalId = setInterval(() => {
+    createPromise(position, delay);
+
+    if (position >= parseInt(amount)) {
+      clearInterval(intervalId);
+      return;
+    }
+    delay += parseInt(step);
+    position += 1;
+  }, parseInt(step));
 }
 
 function handlerForm(e) {
   e.preventDefault();
-  // console.log(e.target);
+  clearInterval(intervalId);
+  clearTimeout(timeoutId);
+  delay = 0;
+  position = 0;
   const formData = new FormData(e.target);
   const formValues = Object.fromEntries(formData.entries());
-  console.log(formValues);
+  const amount = parseInt(formValues.amount);
+  const step = parseInt(formValues.step);
+  delay = parseInt(formValues.delay);
 
-  createPromise();
+  if (!amount || amount <= 0) {
+    Notiflix.Notify.warning(`Amount must be greater than 0`);
+    return;
+  }
+  timeoutId = setTimeout(() => {
+    createInterval(step, amount);
+    position += 1;
+  }, delay);
+  // formRef.reset();
 }
 
 formRef.addEventListener('submit', handlerForm);
