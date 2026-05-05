ed0fe6cc3bc1e1c1b2accb214e07a9de27d54ee4
Zelenro <125198729+Zelenro@users.noreply.github.com>
2026-05-05 09:18:43 +0000

    Deploying to gh-pages from @ Zelenro/hw-09-js@b3cbf596b02bb08bdf5f902e9cb5bed3a2b3e20c 🚀

--- 03-promises.1a52d327.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form").addEventListener("submit",(function(e){e.preventDefault();var t=new FormData(e.target),r=Object.fromEntries(t.entries());console.log(r),Math.random()}));
-//# sourceMappingURL=03-promises.1a52d327.js.map
